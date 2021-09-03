4b1bb62a388375830ce4116fd48e4dc6990d5f19
Daniela Farias <farias.danicris@gmail.com>
2021-09-03 03:37:45 -0300

    Header nas pages existentes

---
+++ pages/order.js
@@ -0,0 +1,33 @@
+import HeaderBar from "../components/HeaderBar";
+import HeaderLogo from "../components/HeaderLogo";
+import AccessButton from "../components/AccessButton";
+import CartButton from "../components/CartButton";
+import OrderButton from "../components/OrderButton";
+import { 
+    Grid,
+    Typography
+} from '@material-ui/core'
+
+export default function Order() {
+    return(
+        <div>
+      <Grid container justifyContent="flex-end">
+        <Grid item>
+          <AccessButton />
+        </Grid>
+      </Grid>
+
+      <Grid container justifyContent="center">
+        <Grid item>
+          <HeaderLogo />
+        </Grid>
+      </Grid>
+
+      <HeaderBar
+        title={<Typography variant='h4'>Meu Pedido</Typography>}
+        icon1={<OrderButton />}
+        icon2={<CartButton />}
+      />
+        </div>
+    );
+}
